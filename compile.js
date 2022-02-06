514b4fa10662b71a725f7cd166485bfaa9bf6e52
Daniel Andrade <dcca12@gmail.com>
2022-02-06 16:09:07 -0300

    compile.js updated

---
+++ compile.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+//find the contract code
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Loterry.sol');
+//read and store the contract content
+const source = fs.readFileSync(lotteryPath, 'utf8');
+
+//config the compiler
+const input = {
+    language: 'Solidity',
+    sources: {
+        'Lottery.sol': {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            },
+        },
+    },
+};
+//execute the compiler and exports
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts[
+    'Lottery.sol'
+].Lottery
